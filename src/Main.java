aaa459be654f69ced450a9b09c357411acb5dc10
Alex <alexsa-8@mail.ru>
2022-11-06 23:31:10 +0300

    номер Амстронга

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-
-
+        ArrayList<Integer> integers = new ArrayList<>();
+        int num = 153;
+        int number = num;
+        int i;
+        int n = 0;
+        while (number >= 1) {
+            i = number % 10;
+            number /= 10;
+            integers.add(i);
+            n++;
+        }
+        int sum = 0;
+        for (Integer j : integers) {
+            sum += Math.pow(j, n);
+        }
+        System.out.println(sum);
+        if (num == sum) {
+            System.out.println(num + " является числом Амстронга.");
+        } else {
+            System.out.println(num + " не является числом Амстронга.");
+        }
     }
 }
